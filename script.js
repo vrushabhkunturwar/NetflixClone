5759fc29808ef5c8095bfef7e8cc43d76ae6ac3b
vrushabhkunturwar <vrushabhkunturwar@gmail.com>
2024-02-06 12:51:41 +0530

    footer completed

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
         { question: 'Where can i watch?', answer: "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere." },
 
-        { question: 'How do i cancle', answer: "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime." },
+        { question: 'How do i cancle?', answer: "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime." },
 
-        { question: 'What can i watch on Netflix', answer: "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want." },
+        { question: 'What can i watch on Netflix?', answer: "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want." },
 
-        { question: 'Is Netflix good for kids', answer: "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see." },
+        { question: 'Is Netflix good for kids?', answer: "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see." },
 
 
         // Add more FAQ items as needed
